82684f0a0d2b3fe0c7f4de89619eaa3992f94649
A-76 <adithyashankar.b@gmail.com>
2023-03-06 16:00:27 -0800

    updated ui

--- interface.py
+++ interface.py
@@ -1,36 +1,55 @@
 import tkinter as tk
-global e1
 
+global e1, frame
+frame = None
 
+RESULTS = ["a","b","d","e","f","g"]
 
-def create_frame(master):
-    print("create frame")
+def search_frame():
+    global e1,frame
+    frame = reset_all()
+    tk.Label(frame, text='Searcher').grid(row=0,column=1)
+    e1 = tk.Entry(frame)
+    e1.bind("<Key>", enter_key_handler)
 
-    frame = Frame(master)
+    e1.grid(row=1, column=1)
 
-    b = Button(frame, text='Do Something')
-    b.pack(pady=10)
+    button = tk.Button(frame, text='Search', width=10,fg="yellow")
+    button.grid(row=1,column=2)
 
-    clearall = Button(frame, text='reset', command=reset_all)
-    clearall.pack(pady=10)
+    button.bind("<Button-1>", click_handler)
+    return
 
-    return frame
+def other_click_handler(event):
+    print("other click")
+    search_frame()
+    return
+
+def display_results_frame(frame):
+    for i in range(len(RESULTS)):
+        tk.Label(frame, text=RESULTS[i]).pack()
+
+    button1 = tk.Button(frame, text='Another Search', width=45,fg="yellow")
+    button1.bind("<Button-1>", other_click_handler)
+    button1.pack()
+    return
 
 def reset_all():
     global frame
-
-    frame.destroy()
-    frame = create_frame(master)
+    if frame is not None:
+        frame.destroy()
+    frame = tk.Frame(master, height = 700, width = 700)
+    frame.pack()
     #frame = create
-    return
+    return frame
 
-def display_results():
-    return
 def click_handler(event):
     global e1
     query = e1.get()
     print(query)
     print("The button was clicked")
+    frame = reset_all()
+    display_results_frame(frame)
     return
 
 def enter_key_handler(event):
@@ -38,20 +57,17 @@ def enter_key_handler(event):
     if(event.char=="p"):
         query = e1.get()
         print(query)
+        frame = reset_all()
+        display_results_frame(frame)
+    return
+
+def initializer():
+    global master
+    master=tk.Tk()
+    search_frame()
+    master.mainloop()
     return
 
 if __name__ == "__main__":
-    print("axd")
-    master=tk.Tk()
-    tk.Label(master, text='Searcher').grid(row=0,column=1)
-    e1 = tk.Entry(master)
-    e1.bind("")
-    e1.bind("<Key>", enter_key_handler)
-
-    e1.grid(row=1, column=1)
-
-    button = tk.Button(master, text='Search', width=10,fg="yellow")
-    button.grid(row=1,column=2)
-
-    button.bind("<Button-1>", click_handler)
-    master.mainloop()
+    initializer()
+    
