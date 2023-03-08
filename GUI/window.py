04cf5ef96d77aa842b026d891523db3a12534d53
Chin-Hung Lee <58142790+NashiKanjou@users.noreply.github.com>
2023-03-09 00:59:00 +0800

    add GUI
    my version of GUI

---
+++ GUI/window.py
@@ -0,0 +1,153 @@
+from tkinter import *
+global entry
+global button
+global window
+global canvas
+def searchbtn_clicked():
+    global entry
+    global canvas
+    keywords = entry.get()
+    clear()
+    #button.configure(command= close_win)
+    #canvas.delete("all")
+    #canvas.destroy()
+    print(keywords)
+    resultpage(keywords, ["result1","result2","result3"])
+
+def clear():
+    global canvas
+    global entry
+    entry.delete(0, END)
+    canvas.destroy()
+    
+def searchpage(): 
+    global entry
+    global button
+    global window
+    global canvas
+
+
+
+    canvas = Canvas(
+        window,
+        bg = "#ffffff",
+        height = 768,
+        width = 1440,
+        bd = 0,
+        highlightthickness = 0,
+        relief = "ridge")
+    canvas.place(x = 0, y = 0)  
+    logo = canvas.create_text(
+        718.5, 284.0,
+        text = "SWE225 Search Engine",
+        fill = "#000000",
+        font = ("None", int(36.0)))
+
+    entry0_img = PhotoImage(file = f"img_textBox0.png")
+    entry0_bg = canvas.create_image(
+        719.5, 382.0,
+        image = entry0_img)
+
+    entry = Entry(
+        bd = 0,
+        bg = "#d9d9d9",
+        highlightthickness = 0,
+        font = ("None", int(24.0)))
+
+    entry.place(
+        x = 139, y = 353,
+        width = 1161,
+        height = 56)
+
+    img0 = PhotoImage(file = f"img0.png")
+    button = Button(
+        image = img0,
+        borderwidth = 0,
+        highlightthickness = 0,
+        command = searchbtn_clicked,
+        relief = "flat")
+    button.place(
+        x = 614, y = 455,
+        width = 210,
+        height = 53)
+    window.mainloop()
+    
+
+
+
+#Result page
+def resultpage(key,results):
+    global entry
+    global button
+    global window
+    global canvas
+    canvas = Canvas(
+        window,
+        bg = "#ffffff",
+        height = 768,
+        width = 1440,
+        bd = 0,
+        highlightthickness = 0,
+        relief = "ridge")
+    canvas.place(x = 0, y = 0)  
+    logo = canvas.create_text(
+        171.0, 76.5,
+        text = "SWE225",
+        fill = "#000000",
+        font = ("None", int(36.0)))
+    resultsBox = canvas.create_text(
+        130.0, 250,
+        text = "results list",
+        anchor = SW,
+        fill = "#000000",
+        font = ("None", int(20.0)))
+        
+    
+    str_result = ""
+    for result in results:
+        str_result = str_result+ key+" " + result +'\n'
+    canvas.itemconfig(resultsBox, text=str_result)
+
+    entry1_img = PhotoImage(file = f"img_textBox1.png")
+    entry1_bg = canvas.create_image(
+        765.0, 76.5,
+        image = entry1_img)
+
+    entry = Entry(
+        bd = 0,
+        bg = "#d9d9d9",
+        highlightthickness = 0,
+        font = ("None", int(24.0)))
+
+    entry.place(
+        x = 296, y = 53,
+        width = 938,
+        height = 45)
+    entry.insert(0, key)
+    img1 = PhotoImage(file = f"img1.png")
+    #button.configure(image= img1)
+    button1 = Button(
+        image = img1,
+        borderwidth = 0,
+        highlightthickness = 0,
+        command = searchbtn_clicked,
+        relief = "flat")
+
+    button1.place(
+        x = 1270, y = 53,
+        width = 86,
+        height = 47)
+    window.mainloop()
+
+def init():
+    global window
+
+    window = Tk()
+
+    window.geometry("1440x768")
+    window.configure(bg = "#ffffff")
+    window.resizable(False, False)
+    searchpage()
+
+if __name__ == "__main__":
+    init()
